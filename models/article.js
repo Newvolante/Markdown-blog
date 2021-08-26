52b2723fc86d2e08e127ad120e21554d27effc23
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-08-26 18:30:49 +0200

    created the schema for the mongo database

---
+++ models/article.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// all the columns of the articles
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  markdown: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now   // no parentheses - called whenever we create a document
+  }
+});
+
+// in this case Article is the collection containing all the documents
+module.exports = mongoose.model('Article', articleSchema);
