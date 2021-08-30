9f5ce180ccffdbfe69596145a2c112f431a1ab71
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-08-30 12:46:42 +0200

    adjusted indentation for server.js

--- server.js
+++ server.js
@@ -6,46 +6,48 @@ const app = express();
 const PORT = 5000;
 
 // connecting to local mongodb database
-mongoose.connect('mongodb://localhost/blog', { 
-  useNewUrlParser: true,      // following deprecation warnings
-  useUnifiedTopology: true    // following deprecation warnings
- });
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, // following deprecation warnings
+  useUnifiedTopology: true // following deprecation warnings
+});
 
 // setting the view engine
 app.set('view engine', 'ejs'); // writing views in ejs
-                               // the first parameter converts views in html
-                               
+// the first parameter converts views in html
+
 // telling express how to access the fields in the _form_fields view
 // used to create and edit articles
-app.use(express.urlencoded( {extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 
 
 // root route
 app.get('/', (req, res, next) => {
-  
+
   // obj containing all the articles
   const articles = [{
-    title: 'Test article',
-    createdAt: new Date(),
-    description: 'Test description'
-  },
-  {
-    title: 'Test article 2',
-    createdAt: new Date(),
-    description: 'Test description 2'
-  },
-  {
-    title: 'Test article 3',
-    createdAt: new Date(),
-    description: 'Test description 3'
-  }
-];
+      title: 'Test article',
+      createdAt: new Date(),
+      description: 'Test description'
+    },
+    {
+      title: 'Test article 2',
+      createdAt: new Date(),
+      description: 'Test description 2'
+    },
+    {
+      title: 'Test article 3',
+      createdAt: new Date(),
+      description: 'Test description 3'
+    }
+  ];
 
-// rendering the root view ('index.ejs') from the views folder
-// passing an obj to show all the articles
-res.render('articles/index', {
-  articles: articles
-});
+  // rendering the root view ('index.ejs') from the views folder
+  // passing an obj to show all the articles
+  res.render('articles/index', {
+    articles: articles
+  });
 })
 
 // articles route
