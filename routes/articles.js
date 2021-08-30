3a93013051cbda6522872d0bb4e4c7d5a63f7eac
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-08-30 12:32:06 +0200

    exiting the filled new article page and going back to it no longer throws an error

--- routes/articles.js
+++ routes/articles.js
@@ -5,8 +5,11 @@ const router = express.Router();
 
 // manages /articles/new
 router.get('/new', (req, res) => {
-  // renders the the new.ejs view
-  res.render('articles/new');
+  // 1 - renders the the new.ejs view
+  // 2 - passing a new article as a property makes it work when
+  // 2 - you exit the filled new article page without sending it
+  // 2 - and you go back to it
+  res.render('articles/new', { article: new Article() });
 })
 
 // whenever requesting ./articles/"something"
